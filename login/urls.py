df7907de87a25ebc156aaa89e45114fc4970036c
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-06-26 09:54:15 -0300

    Remove view de teste
    Co-authored-by: ebarbara <estevan.barbara@gmail.com>

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from login.views import LoginView, TestView
+from login.views import LoginView
 
 app_name = 'login'
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-    path('teste', TestView.as_view(), name='teste')
 ]
--- login/views.py
+++ login/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from login.sca import authenticate
-from login.decorators import auth_required
 
 
 class LoginView(APIView):
@@ -25,10 +24,3 @@ class LoginView(APIView):
             return Response(token, status=200)
 
         return Response('Usuário ou senha incorretos', status=403)
-
-
-class TestView(APIView):
-
-    @auth_required
-    def get(self, request, *args, **kwargs):
-        return Response('Usuário Logado', status=200)
