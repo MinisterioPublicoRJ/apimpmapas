5996cc7b0a7840dacdfd378758d30887b075b251
ebarbara <estevan.barbara@gmail.com>
2019-07-09 11:53:41 -0300

    Testes de integração com os acessos ao oracle e ao bda
    Co-authored-by: rhenanbartels <rhenan.bartels@gmail.com>

--- api/tests/test_db_integration.py
+++ api/tests/test_db_integration.py
@@ -35,3 +35,67 @@ class IntegrationTest(TestCase):
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp_json['external_data'], [[7625679.0, 8364250.0]])
+
+    def test_integration_view_oracle(self):
+        if 'INTEGRATION_TEST' not in os.environ:
+            self.skipTest(
+                "Integration tests shouldn't be run along with unit tests"
+            )
+
+        make(
+            'api.Dado',
+            id=3,
+            database='ORA',
+            columns=['ORGI_NM_ORGAO'],
+            id_column='ORGI_CDORGAO',
+            schema='ORGI',
+            table='ORGI_ORGAO'
+        )
+        url = reverse(
+            'api:detail_dado',
+            kwargs={
+                'domain_id': 200493,
+                'pk': 3
+            }
+        )
+
+        resp = self.client.get(url)
+        resp_json = resp.json()
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp_json['external_data'],
+            [['1ª PROMOTORIA DE JUSTIÇA DA INFÂNCIA E DA JUVENTUDE DE BELFORD ROXO']]
+        )
+
+    def test_integration_view_bda(self):
+        if 'INTEGRATION_TEST' not in os.environ:
+            self.skipTest(
+                "Integration tests shouldn't be run along with unit tests"
+            )
+
+        make(
+            'api.Dado',
+            id=3,
+            database='BDA',
+            columns=['orgi_nm_orgao'],
+            id_column='orgi_cdorgao',
+            schema='exadata',
+            table='orgi_orgao'
+        )
+        url = reverse(
+            'api:detail_dado',
+            kwargs={
+                'domain_id': 200493,
+                'pk': 3
+            }
+        )
+
+        resp = self.client.get(url)
+        resp_json = resp.json()
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp_json['external_data'],
+            [['1ª PROMOTORIA DE JUSTIÇA DA INFÂNCIA E DA JUVENTUDE DE BELFORD ROXO']]
+        )
