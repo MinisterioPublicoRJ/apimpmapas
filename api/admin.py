9f1d8bb5fbffa410d83cb7c7cb7207f0853a8247
ebarbara <estevan.barbara@gmail.com>
2019-07-19 18:21:09 -0300

    Correção na página de admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
 
 from api.models import Dado, Icone, Entidade
 
 
-admin.site.register(Dado)
+class DadoAdmin(OrderedModelAdmin):
+    list_display = ('title', 'entity_type', 'move_up_down_links')
+    list_filter = ('entity_type__name',)
+
+
+admin.site.register(Dado, DadoAdmin)
 admin.site.register(Icone)
 admin.site.register(Entidade)
