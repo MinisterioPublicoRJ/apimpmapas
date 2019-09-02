5ca93ef148b75268b0f4f7c2899b356d0f2f9450
ebarbara <estevan.barbara@gmail.com>
2019-09-02 12:17:02 -0300

    Configuração dos roteadores de banco de dados

--- lupa/lupa_router.py
+++
@@ -1,9 +0,0 @@
-class LupaRouter:
-    """
-    Router para controlar operações de banco de dados no app Lupa.
-    """
-
-    def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'lupa':
-            return 'default'
-        return None
