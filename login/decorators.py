299d331a4dff1c8ff6b97357903bf2343d351395
ebarbara <estevan.barbara@gmail.com>
2019-09-05 14:32:18 -0300

    Acesso livre a caixinhas sem lista de grupos

--- login/decorators.py
+++ login/decorators.py
@@ -1,10 +1,11 @@
 from functools import wraps
 
-import jwt
-
-from rest_framework.response import Response
 from decouple import config
+import jwt
 from jwt.exceptions import InvalidSignatureError, DecodeError
+from rest_framework.response import Response
+
+from lupa.models import Dado
 
 
 def auth_required(func):
@@ -13,12 +14,22 @@ def auth_required(func):
         request = args[1]
         token = request.GET.get('auth_token')
 
+        dado = Dado.objects.get(pk=kwargs['pk'])
+        restricted_to = []
+        for role in dado.roles_allowed.all().values_list('role', flat=True):
+            restricted_to.append(role)
+
+        if not restricted_to:
+            return func(*args, **kwargs)
+
         try:
             token = jwt.decode(
                 token,
                 config('SECRET_KEY'),
                 algorithms=["HS256"]
             )
+            # TODO checagem de permissões
+            # permissions = token['permissions']
             return func(*args, **kwargs)
         except (InvalidSignatureError, DecodeError):
             return Response({}, status=403)
