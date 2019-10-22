7fd12da18340a0abdb5fbc39b11acc3df30f7e34
rhenanbartels <rhenan.bartels@gmail.com>
2019-10-22 15:30:56 -0300

    Cria Command para popular cache de entidades

--- lupa/tests/test_commands.py
+++ lupa/tests/test_commands.py
@@ -9,8 +9,8 @@ from lupa.management.commands.checkconsistency import (
     Command,
     parsecolumns
 )
-from lupa.models import ColunaDado, Dado
-from lupa.serializers import DadoSerializer
+from lupa.models import ColunaDado, Dado, Entidade
+from lupa.serializers import DadoSerializer, EntidadeSerializer
 
 
 class TestCheckConsistency(TestCase):
@@ -247,29 +247,46 @@ class TestCheckConsistency(TestCase):
 
 
 class UpdateCache(TestCase):
-    @mock.patch('lupa.management.commands.update_cache.repopulate_cache')
+    @mock.patch('lupa.management.commands.update_cache._repopulate_cache_data')
     def test_update_data_cache(self, _repopulate_cache):
         key_prefix = 'lupa_dado'
-        dado_1 = make(
+        make(
             'lupa.Dado',
             cache_timeout=7,
             last_cache_update=dt(2019, 10, 15, 12, 0, 0),
         )
-        dado_2 = make(
+        make(
             'lupa.Dado',
             cache_timeout=7,
             last_cache_update=dt(2019, 10, 14, 12, 0, 0),
         )
 
-        call_command('update_cache')
+        call_command('update_cache', 'dado')
+
         expiring_data = Dado.cache.expiring()
-        args_prefix, args_entities, args_queryset, args_serializer\
+        args_prefix, args_queryset, args_serializer\
             = _repopulate_cache.call_args_list[0][0]
 
         self.assertEqual(args_prefix, key_prefix)
-        self.assertEqual(expiring_data[0].pk, args_queryset[0].pk)
-        self.assertCountEqual(
-            args_entities,
-            [dado_1.entity_type, dado_2.entity_type]
-        )
+        self.assertCountEqual(expiring_data, args_queryset)
         self.assertTrue(args_serializer is DadoSerializer)
+
+    @mock.patch('lupa.management.commands.update_cache._repopulate_cache_entity')
+    def test_update_entity_cache(self, _repopulate_cache):
+        key_prefix = 'lupa_entidade'
+
+        make(
+            'lupa.Entidade',
+            _quantity=2,
+            last_cache_update=dt(2019, 10, 10)
+        )
+
+        call_command('update_cache', 'entidade')
+
+        expiring_entity = Entidade.cache.expiring()
+        args_prefix, args_queryset, args_serializer\
+            = _repopulate_cache.call_args_list[0][0]
+
+        self.assertEqual(args_prefix, key_prefix)
+        self.assertCountEqual(expiring_entity, args_queryset)
+        self.assertTrue(args_serializer is EntidadeSerializer)
