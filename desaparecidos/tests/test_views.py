8936e98f4f89ab728dad7212a525cea53a94a328
rhenanbartels <rhenan.bartels@gmail.com>
2019-12-09 10:30:28 -0300

    Resposta de pedido sendo processsado

--- desaparecidos/tests/test_views.py
+++ desaparecidos/tests/test_views.py
@@ -22,3 +22,21 @@ class TestDesaparecidos(TestCase):
         _async_calculate_rank.assert_called_once_with(id_sinalid)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data, expected_resp)
+
+    @mock.patch('desaparecidos.views.cache')
+    @mock.patch('desaparecidos.views.async_calculate_rank')
+    def test_search_processing_request(self, _async_calculate_rank, _cache):
+        cache_resp = {'status': 'processing'}
+        _cache.get.return_value = cache_resp
+        id_sinalid = '12345'
+        url = reverse(
+            'desaparecidos:busca',
+            kwargs={'id_sinalid': id_sinalid}
+        )
+
+        resp = self.client.post(url)
+
+        _cache.get.assert_called_once_with(id_sinalid)
+        _async_calculate_rank.assert_not_called()
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, cache_resp)
--- desaparecidos/views.py
+++ desaparecidos/views.py
@@ -7,10 +7,9 @@ from desaparecidos.tasks import async_calculate_rank
 
 class DesaparecidosView(APIView):
     def post(self, request, *args, **kwargs):
-        data = None
         id_sinalid = kwargs.pop('id_sinalid')
-        cache_resp = cache.get(id_sinalid)
-        if cache_resp is None:
+        data = cache.get(id_sinalid)
+        if data is None:
             async_calculate_rank(id_sinalid)
             data = {'status': 'Seu pedido será processado'}
 
