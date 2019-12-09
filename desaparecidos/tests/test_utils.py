9bc753b4fdf13776167866102c732471c88a950c
rhenanbartels <rhenan.bartels@gmail.com>
2019-12-09 17:25:01 -0300

    Corrige função de paginação

--- desaparecidos/tests/test_utils.py
+++ desaparecidos/tests/test_utils.py
@@ -5,34 +5,43 @@ from desaparecidos.utils import paginate
 
 class TestUtils(TestCase):
     def test_paginate_data(self):
-        data = {'key 1': list(range(20)), 'key 2': list(range(20, 40))}
+        data = {
+            'key 1': dict(zip(range(10), range(20))),
+            'key 2': dict(zip(range(10), range(20, 40)))
+        }
 
         page_data = paginate(data, page=1, page_size=10)
 
         expected_data = {
-            'key 1': list(range(10)),
-            'key 2': list(range(20, 30))
+            'key 1': dict(zip(range(10), range(10))),
+            'key 2': dict(zip(range(10), range(20, 30)))
         }
         self.assertEqual(page_data, expected_data)
 
     def test_paginate_data_2(self):
-        data = {'key 1': list(range(100)), 'key 2': list(range(100, 200))}
+        data = {
+            'key 1': dict(zip(range(100), range(100))),
+            'key 2': dict(zip(range(100), range(100, 200)))
+        }
 
         page_data = paginate(data, page=2, page_size=10)
 
         expected_data = {
-            'key 1': list(range(10, 20)),
-            'key 2': list(range(110, 120))
+            'key 1': dict(zip(range(10), range(10, 20))),
+            'key 2': dict(zip(range(10), range(110, 120)))
         }
         self.assertEqual(page_data, expected_data)
 
     def test_paginate_data_page_size(self):
-        data = {'key 1': list(range(100)), 'key 2': list(range(100, 200))}
+        data = {
+            'key 1': dict(zip(range(100), range(100))),
+            'key 2': dict(zip(range(100), range(100, 200)))
+        }
 
         page_data = paginate(data, page=3, page_size=20)
 
         expected_data = {
-            'key 1': list(range(40, 60)),
-            'key 2': list(range(140, 160))
+            'key 1': dict(zip(range(20), range(40, 60))),
+            'key 2': dict(zip(range(20), range(140, 160)))
         }
         self.assertEqual(page_data, expected_data)
--- desaparecidos/utils.py
+++ desaparecidos/utils.py
@@ -1,2 +1,7 @@
-def paginate(data, page, page_size):
-    return {k: v[page_size*(page-1):page_size*page] for k, v in data.items()}
+def paginate(data, page, page_size=10):
+    new_dict = dict()
+    for k, v in data.items():
+        slice_ = range(page_size*(page-1), page_size*page)
+        new_dict[k] = {new_idx: v[val] for new_idx, val in enumerate(slice_)}
+
+    return new_dict
