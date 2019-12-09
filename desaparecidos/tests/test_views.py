f74107b55a2d3a0f8ae82e6ff9ac22cbe638bb01
rhenanbartels <rhenan.bartels@gmail.com>
2019-12-09 10:54:48 -0300

    Retorna 404 se identificador não existe

--- desaparecidos/tests/test_views.py
+++ desaparecidos/tests/test_views.py
@@ -5,9 +5,12 @@ from django.test import TestCase
 
 
 class TestDesaparecidos(TestCase):
+    @mock.patch('desaparecidos.views.search_target_person')
+    @mock.patch('desaparecidos.views.client')
     @mock.patch('desaparecidos.views.cache')
     @mock.patch('desaparecidos.views.async_calculate_rank')
-    def test_search_id_sinalid(self, _async_calculate_rank, _cache):
+    def test_search_id_sinalid(self, _async_calculate_rank, _cache, _client,
+                               _search):
         _cache.get.return_value = None
         id_sinalid = '12345'
         url = reverse(
@@ -27,9 +30,12 @@ class TestDesaparecidos(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data, expected_resp)
 
+    @mock.patch('desaparecidos.views.search_target_person')
+    @mock.patch('desaparecidos.views.client')
     @mock.patch('desaparecidos.views.cache')
     @mock.patch('desaparecidos.views.async_calculate_rank')
-    def test_search_processing_request(self, _async_calculate_rank, _cache):
+    def test_search_processing_request(self, _async_calculate_rank, _cache,
+                                       _client, _search):
         cache_resp = {'status': 'processing'}
         _cache.get.return_value = cache_resp
         id_sinalid = '12345'
@@ -45,9 +51,12 @@ class TestDesaparecidos(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data, cache_resp)
 
+    @mock.patch('desaparecidos.views.search_target_person')
+    @mock.patch('desaparecidos.views.client')
     @mock.patch('desaparecidos.views.cache')
     @mock.patch('desaparecidos.views.async_calculate_rank')
-    def test_search_ready(self, _async_calculate_rank, _cache):
+    def test_search_ready(self, _async_calculate_rank, _cache, _client,
+                          _search):
         cache_resp = {'status': 'ready', 'data': [1, 2, 3, 4]}
         _cache.get.return_value = cache_resp
         id_sinalid = '12345'
@@ -61,4 +70,30 @@ class TestDesaparecidos(TestCase):
         _cache.get.assert_called_once_with(id_sinalid)
         _async_calculate_rank.delay.assert_not_called()
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, cache_resp)
+
+    @mock.patch('desaparecidos.views.search_target_person')
+    @mock.patch('desaparecidos.views.client')
+    @mock.patch('desaparecidos.views.cache')
+    @mock.patch('desaparecidos.views.async_calculate_rank')
+    def test_search_id_not_found(self, _async_calculate_rank, _cache, _client,
+                                 _search):
+        cursor_mock = mock.MagicMock()
+        _client.return_value = cursor_mock
+        _search.return_value = None
+        cache_resp = {'status': 'Identificador Sinalid não encontrado'}
+        _cache.get.return_value = cache_resp
+        id_sinalid = 'not exist'
+        url = reverse(
+            'desaparecidos:busca',
+            kwargs={'id_sinalid': id_sinalid}
+        )
+
+        resp = self.client.post(url)
+
+        _client.assert_called_once_with()
+        _search.assert_called_once_with(cursor_mock, id_sinalid)
+        _cache.get.assert_not_called()
+        _async_calculate_rank.delay.assert_not_called()
+        self.assertEqual(resp.status_code, 404)
         self.assertEqual(resp.data, cache_resp)
--- desaparecidos/views.py
+++ desaparecidos/views.py
@@ -2,16 +2,25 @@ from django.core.cache import cache
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from busca_desaparecidos.dao import client, search_target_person
+
 from desaparecidos.tasks import async_calculate_rank
 
 
 class DesaparecidosView(APIView):
     def post(self, request, *args, **kwargs):
         id_sinalid = kwargs.pop('id_sinalid')
-        data = cache.get(id_sinalid)
+        cursor = client()
+        person = search_target_person(cursor, id_sinalid)
+        if person is None:
+            data, status\
+                = {'status': 'Identificador Sinalid não encontrado'}, 404
+        else:
+            data, status = cache.get(id_sinalid), 200
+
         if data is None:
             async_calculate_rank.delay(id_sinalid)
             cache.set(id_sinalid, {'status': 'processing'})
-            data = {'status': 'Seu pedido será processado'}
+            data, status = {'status': 'Seu pedido será processado'}, 200
 
-        return Response(data)
+        return Response(data, status=status)
