e79acdd2082952d0009bb064194bdfbeffe3ff3c
rhenanbartels <rhenan.bartels@gmail.com>
2019-12-09 10:21:25 -0300

    Inicia busca assíncrona

--- desaparecidos/tests/test_views.py
+++ desaparecidos/tests/test_views.py
@@ -1,14 +1,19 @@
+from unittest import mock
+
 from django.shortcuts import reverse
 from django.test import TestCase
 
 
 class TestDesaparecidos(TestCase):
-    def test_search_id_sinalid(self):
+    @mock.patch('desaparecidos.views.async_calculate_rank')
+    def test_search_id_sinalid(self, _async_calculate_rank):
+        id_sinalid = '12345'
         url = reverse(
             'desaparecidos:busca',
-            kwargs={'id_sinalid': '12345'}
+            kwargs={'id_sinalid': id_sinalid}
         )
 
         resp = self.client.post(url)
 
+        _async_calculate_rank.assert_called_once_with(id_sinalid)
         self.assertEqual(resp.status_code, 200)
--- desaparecidos/views.py
+++ desaparecidos/views.py
@@ -1,7 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from desaparecidos.tasks import async_calculate_rank
+
 
 class DesaparecidosView(APIView):
     def post(self, request, *args, **kwargs):
+        id_sinalid = kwargs.pop('id_sinalid')
+        async_calculate_rank(id_sinalid)
         return Response()
