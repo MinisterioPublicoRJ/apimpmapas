9ae93e741e14ba20bd7ebcefb729e2afffd44998
rhenanbartels <rhenan.bartels@gmail.com>
2019-12-10 10:21:47 -0300

    Refatora desaparecidos/views.py

--- desaparecidos/views.py
+++ desaparecidos/views.py
@@ -9,6 +9,14 @@ from desaparecidos.tasks import async_calculate_rank
 from desaparecidos.utils import paginate
 
 
+def _client():
+    return client(
+        config('DESAPARECIDOS_DB_USER'),
+        config('DESAPARECIDOS_DB_PWD'),
+        config('DESAPARECIDOS_DB_HOST')
+    )
+
+
 def _paginate(data, page):
     try:
         page = int(page)
@@ -21,24 +29,22 @@ def _paginate(data, page):
 class DesaparecidosView(APIView):
     def get(self, request, *args, **kwargs):
         id_sinalid = kwargs.pop('id_sinalid')
-        cursor = client(
-            config('DESAPARECIDOS_DB_USER'),
-            config('DESAPARECIDOS_DB_PWD'),
-            config('DESAPARECIDOS_DB_HOST')
-        )
+        cursor = _client()
         person = search_target_person(cursor, id_sinalid)
+
         if person is None:
             data, status\
                 = {'status': 'Identificador Sinalid não encontrado'}, 404
         else:
             data, status = cache.get(id_sinalid), 200
+
         if data is None:
             async_calculate_rank.delay(id_sinalid, person)
             cache.set(id_sinalid, {'status': 'processing'})
             data, status = {'status': 'Seu pedido será processado'}, 201
-        else:
-            page = int(request.GET.get('page', 1))
-            if data['status'] == 'ready':
-                data['data'] = _paginate(data['data'], page)
+
+        if data['status'] == 'ready':
+            page = request.GET.get('page', 1)
+            data['data'] = _paginate(data['data'], page)
 
         return Response(data, status=status)
