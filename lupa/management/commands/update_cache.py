88b8d48404373921d5d25e1f6594c429c41190ca
rhenanbartels <rhenan.bartels@gmail.com>
2020-01-08 14:54:55 -0300

    Refatora command update_cache

--- lupa/management/commands/update_cache.py
+++ lupa/management/commands/update_cache.py
@@ -10,34 +10,48 @@ from lupa.cache import (_repopulate_cache_data_entity,
 from lupa.models import Entidade, DadoEntidade, DadoDetalhe
 
 
+class CacheOption:
+    def __init__(self, option):
+        self._obj, self._prefix, self._action = self.pre_init(option)
+
+    def pre_init(self, option):
+        options = {
+            'entidade': (
+                Entidade,
+                ENTITY_KEY_PREFIX,
+                _repopulate_cache_entity
+            ),
+            'dado_entidade': (
+                DadoEntidade,
+                DATA_ENTITY_KEY_PREFIX,
+                _repopulate_cache_data_entity
+            ),
+            'dado_detalhe': (
+                DadoDetalhe,
+                DATA_DETAIL_KEY_PREFIX,
+                _repopulate_cache_data_detail
+            )
+        }
+
+        if option not in options:
+            raise ValueError(
+                'Opção "%s" inválida. Escolha entre: %s'
+                % (option, list(options))
+            )
+
+        return options[option]
+
+    def queryset(self):
+        return self._obj.cache.expiring()
+
+    def repopulate(self):
+        self._action(self._prefix, self.queryset())
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('obj_type', type=str)
 
     def handle(self, *args, **options):
-        obj_type = options['obj_type']
-        if obj_type == 'dado_entidade':
-            queryset = DadoEntidade.cache.expiring()
-            _repopulate_cache_data_entity(
-                DATA_ENTITY_KEY_PREFIX,
-                queryset
-            )
-        elif obj_type == 'dado_detalhe':
-            queryset = DadoDetalhe.cache.expiring()
-            _repopulate_cache_data_detail(
-                DATA_DETAIL_KEY_PREFIX,
-                queryset
-            )
-
-        elif obj_type == 'entidade':
-            queryset = Entidade.cache.expiring()
-            _repopulate_cache_entity(
-                ENTITY_KEY_PREFIX,
-                queryset
-            )
-
-        else:
-            raise ValueError(
-                'Opção %s inválida.'
-                ' Escolha entre "dado_entidade", "dado_detalhe" e "entidade"'
-            )
+        cache_option = CacheOption(options['obj_type'])
+        cache_option.repopulate()
