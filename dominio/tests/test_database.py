2f51cb4805fc8dd4503909ac71c22cc656c23d1d
ebarbara <estevan.barbara@gmail.com>
2020-02-06 18:33:32 -0300

    Testes do acesso ao Impala

--- dominio/tests/test_database.py
+++ dominio/tests/test_database.py
@@ -2,7 +2,10 @@ from unittest import mock
 
 from django.test import TestCase
 
+
 from mprj_api.db_routers import DominioRouter
+from dominio.db_connectors import BDA_Error, execute
+from lupa.exceptions import QueryError
 
 
 class DominioDbTest(TestCase):
@@ -14,3 +17,30 @@ class DominioDbTest(TestCase):
 
         result = router.db_for_read(model)
         self.assertEqual(result, 'dominio_db')
+
+    @mock.patch('dominio.db_connectors.bda_connect')
+    def test_impala_query(self, _bda_connect):
+        query = 'MOCK QUERY'
+
+        cursor = mock.MagicMock()
+        _bda_connect.return_value.__enter__\
+            .return_value.cursor.return_value.__enter__\
+            .return_value = cursor
+        execute(query)
+
+        cursor.execute.assert_called_once_with(query)
+
+    @mock.patch('dominio.db_connectors.bda_connect')
+    def test_impala_query_fail(self, _bda_connect):
+        query = 'MOCK QUERY'
+
+        cursor = mock.MagicMock()
+        cursor.execute.side_effect = BDA_Error('test error')
+
+        with self.assertRaises(QueryError):
+            _bda_connect.return_value.__enter__\
+                .return_value.cursor.return_value.__enter__\
+                .return_value = cursor
+            execute(query)
+
+        cursor.execute.assert_called_once_with(query)
