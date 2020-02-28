f3dcfc4ad5d634dcb903089ae42e16a64cb79dae
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-02-28 17:13:55 -0300

    Usa prepared statements para as queries

--- dominio/views.py
+++ dominio/views.py
@@ -39,7 +39,8 @@ class DetalheAcervoView(APIView):
         ]
         return result_list[:n]
 
-    def get_acervo_increase(self, orgao_id, dt_inicio, dt_fim):
+    @staticmethod
+    def get_acervo_increase(orgao_id, dt_inicio, dt_fim):
         query = """
                 WITH tb_acervo_orgao_pct as (
                     SELECT *
@@ -47,7 +48,7 @@ class DetalheAcervoView(APIView):
                     INNER JOIN (
                         SELECT cod_pct
                         FROM {namespace}.atualizacao_pj_pacote
-                        WHERE id_orgao = {orgao_id}
+                        WHERE id_orgao = :orgao_id
                         ) org
                     ON org.cod_pct = ac.cod_atribuicao)
                 SELECT
@@ -61,7 +62,7 @@ class DetalheAcervoView(APIView):
                     INNER JOIN {namespace}.tb_regra_negocio_investigacao regras
                         ON regras.cod_atribuicao = acpc.cod_atribuicao
                         AND regras.classe_documento = acpc.tipo_acervo
-                    WHERE dt_inclusao = to_timestamp('{dt_fim}', 'yyyy-MM-dd')
+                    WHERE dt_inclusao = to_timestamp(:dt_fim, 'yyyy-MM-dd')
                     GROUP BY cod_orgao
                     ) tb_data_fim
                 INNER JOIN (
@@ -70,17 +71,17 @@ class DetalheAcervoView(APIView):
                     INNER JOIN {namespace}.tb_regra_negocio_investigacao regras
                         ON regras.cod_atribuicao = acpc.cod_atribuicao
                         AND regras.classe_documento = acpc.tipo_acervo
-                    WHERE dt_inclusao = to_timestamp('{dt_inicio}', 'yyyy-MM-dd')
+                    WHERE dt_inclusao = to_timestamp(:dt_inicio, 'yyyy-MM-dd')
                     GROUP BY cod_orgao
                     ) tb_data_inicio
                 ON tb_data_fim.cod_orgao = tb_data_inicio.cod_orgao
-                """.format(
-                    namespace=settings.TABLE_NAMESPACE,
-                    orgao_id=orgao_id,
-                    dt_inicio=dt_inicio,
-                    dt_fim=dt_fim
-                )
-        return run_query(query)
+                """.format(namespace=settings.TABLE_NAMESPACE)
+        parameters = {
+            'orgao_id': orgao_id,
+            'dt_inicio': dt_inicio,
+            'dt_fim': dt_fim
+        }
+        return run_query(query, parameters)
 
     def get(self, request, *args, **kwargs):
         orgao_id = int(self.kwargs['orgao_id'])
@@ -123,14 +124,16 @@ class OutliersView(APIView):
             "INNER JOIN {namespace}.tb_regra_negocio_investigacao C "
             "ON C.cod_atribuicao = B.cod_pct "
             "AND C.classe_documento = A.tipo_acervo "
-            "WHERE cod_orgao = {orgao_id} "
-            "AND dt_inclusao = to_timestamp('{data}', 'yyyy-MM-dd')"
+            "WHERE cod_orgao = :orgao_id "
+            "AND dt_inclusao = to_timestamp(:data, 'yyyy-MM-dd')"
             .format(
-                namespace=settings.TABLE_NAMESPACE,
-                orgao_id=orgao_id,
-                data=data
+                namespace=settings.TABLE_NAMESPACE
             ))
-        return run_query(query)
+        parameters = {
+            'orgao_id': orgao_id,
+            'data': data
+        }
+        return run_query(query, parameters)
 
     def get_outliers(self, orgao_id, dt_calculo):
 
@@ -149,14 +152,16 @@ class OutliersView(APIView):
                 INNER JOIN {namespace}.tb_distribuicao B
                 ON A.cod_atribuicao = B.cod_atribuicao
                 AND A.dt_inclusao = B.dt_inclusao
-                WHERE A.cod_orgao = {orgao_id}
-                AND B.dt_inclusao = to_timestamp('{dt_calculo}', 'yyyy-MM-dd')
+                WHERE A.cod_orgao = :orgao_id
+                AND B.dt_inclusao = to_timestamp(:dt_calculo, 'yyyy-MM-dd')
                 """.format(
-                    namespace=settings.TABLE_NAMESPACE,
-                    orgao_id=orgao_id,
-                    dt_calculo=dt_calculo
+                    namespace=settings.TABLE_NAMESPACE
                 )
-        return run_query(query)
+        parameters = {
+            'orgao_id': orgao_id,
+            'dt_calculo': dt_calculo
+        }
+        return run_query(query, parameters)
 
     def get(self, request, *args, **kwargs):
         orgao_id = int(self.kwargs['orgao_id'])
@@ -196,13 +201,15 @@ class SaidasView(APIView):
         query = """
                 SELECT saidas, id_orgao, cod_pct, percent_rank, dt_calculo
                 FROM {namespace}.tb_saida
-                WHERE id_orgao = {orgao_id}
+                WHERE id_orgao = :orgao_id
                 """.format(
-                    orgao_id=orgao_id,
                     namespace=settings.TABLE_NAMESPACE
                 )
+        parameters = {
+            'orgao_id': orgao_id
+        }
 
-        return run_query(query)
+        return run_query(query, parameters)
 
     def get(self, request, *args, **kwargs):
         orgao_id = int(self.kwargs['orgao_id'])
@@ -249,15 +256,17 @@ class EntradasView(APIView):
                     lout,
                     hout
                 FROM {namespace}.tb_dist_entradas
-                WHERE comb_orga_dk = {orgao_id}
-                AND comb_cpf = '{nr_cpf}'
+                WHERE comb_orga_dk = :orgao_id
+                AND comb_cpf = :nr_cpf
                 """.format(
-                    orgao_id=orgao_id,
-                    nr_cpf=nr_cpf,
                     namespace=settings.TABLE_NAMESPACE
                 )
+        parameters = {
+            'orgao_id': orgao_id,
+            'nr_cpf': nr_cpf
+        }
 
-        return run_query(query)
+        return run_query(query, parameters)
 
     def get(self, request, *args, **kwargs):
         orgao_id = int(self.kwargs['orgao_id'])
