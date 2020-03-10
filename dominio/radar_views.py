99edcca6058cf3b29b63b85dbd2ba016b1a67e9e
rhenanbartels <rhenan.bartels@gmail.com>
2020-03-10 12:07:10 -0300

    Adiciona cache_page ao endpoint radar

--- dominio/radar_views.py
+++ dominio/radar_views.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,6 +8,12 @@ from dominio.db_connectors import run_query
 from dominio.radar_queries import field_names, query
 
 
+@method_decorator(
+    cache_page(
+        settings.CACHE_TIMEOUT,
+        key_prefix="dominio_radar"),
+    name="dispatch"
+)
 class SuaPromotoriaView(APIView):
     def get_radar_data(self, orgao_id):
         f_query = query.format(
