65f6c197d40dc80d2dbc535bb201dbe8af3112d8
rhenanbartels <rhenan.bartels@gmail.com>
2020-03-11 16:10:56 -0300

    Formata nome das promotorias do topo

--- dominio/radar_views.py
+++ dominio/radar_views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 
 from dominio.db_connectors import run_query
 from dominio.radar_queries import field_names, query
+from dominio.suamesa import format_text
 
 
 @method_decorator(
@@ -16,18 +17,49 @@ from dominio.radar_queries import field_names, query
     name="dispatch"
 )
 class SuaPromotoriaView(APIView):
+    def prepare_response(self, resp):
+        resp["nm_max_arquivamentos"] = format_text(
+            resp["nm_max_arquivamentos"]
+        )
+        resp["nm_max_indeferimentos"] = format_text(
+            resp["nm_max_indeferimentos"]
+        )
+        resp["nm_max_instauracoes"] = format_text(
+            resp["nm_max_instauracoes"]
+        )
+        resp["nm_max_tac"] = format_text(
+            resp["nm_max_tac"]
+        )
+        resp["nm_max_acoes"] = format_text(
+            resp["nm_max_acoes"]
+        )
+
+        return resp
+
     def get_radar_data(self, orgao_id):
         f_query = query.format(
-                orgao_id=orgao_id,
                 schema=settings.TABLE_NAMESPACE
-            )
+        )
+        parameters = {"orgao_id": orgao_id}
 
-        return run_query(f_query)
+        return run_query(f_query, parameters=parameters)
+
+    def parse_orgao_id(self, orgao_id):
+        try:
+            orgao_id = int(orgao_id)
+        except ValueError:
+            raise Http404("Valor <orgao_id> inválido")
+
+        return orgao_id
 
     def get(self, request, *args, **kwargs):
-        orgao_id = kwargs.get("orgao_id")
+        orgao_id = self.parse_orgao_id(kwargs.get("orgao_id"))
+
         radar_data = self.get_radar_data(orgao_id)
         if radar_data is None:
             raise Http404
 
-        return Response(data=dict(zip(field_names, radar_data[0])))
+        resp_data = self.prepare_response(
+            dict(zip(field_names, radar_data[0]))
+        )
+        return Response(data=resp_data)
--- dominio/tests/test_radar_views.py
+++ dominio/tests/test_radar_views.py
@@ -15,50 +15,89 @@ class TestSuaPromotoria(TestCase):
     def test_correct_response(self, _run_query):
 
         run_query_resp = [
-            (
-                'Tutela Coletiva',
-                Decimal('123456'),
-                179,
-                25,
-                12,
-                0,
-                0,
-                datetime(2019, 3, 9, 14, 50, 11, 324000),
-                28,
-                278,
-                151,
-                65,
-                10,
-                21,
-                149,
-                3,
-                70,
-                32,
-                4,
-                64.38848920863309,
-                0.0,
-                16.55629139072848,
-                18.461538461538463,
-                0.0,
-                0.20134228187919462,
-                0.0,
-                -0.6428571428571429,
-                -0.625,
-                0.0
-            )
+            ('Tutela Coletiva',
+             Decimal('123456'),
+             179,
+             25,
+             12,
+             0,
+             0,
+             datetime(2020, 3, 9, 14, 50, 11, 324000),
+             '1ª PROMOTORIA DE JUSTIÇA',
+             278,
+             '2ª PROMOTORIA DE JUSTIÇA',
+             151,
+             '3ª PROMOTORIA DE JUSTIÇA',
+             65,
+             '4ª PROMOTORIA DE JUSTIÇA',
+             10,
+             '5ª PROMOTORIA DE JUSTIÇA',
+             21,
+             28,
+             149,
+             3,
+             70,
+             26,
+             4,
+             64.38848920863309,
+             0.0,
+             16.55629139072848,
+             18.461538461538463,
+             0.0,
+             0.20134228187919462,
+             0.0,
+             -0.6428571428571429,
+             -0.5384615384615384,
+             0.0)
         ]
         _run_query.return_value = run_query_resp
 
         url = reverse('dominio:radar', args=('1', ))
-        expected_data = dict(zip(field_names, run_query_resp[0]))
+        expected_data = (
+            'Tutela Coletiva',
+            Decimal('123456'),
+            179,
+            25,
+            12,
+            0,
+            0,
+            datetime(2020, 3, 9, 14, 50, 11, 324000),
+            '1ª Promotoria de Justiça',
+            278,
+            '2ª Promotoria de Justiça',
+            151,
+            '3ª Promotoria de Justiça',
+            65,
+            '4ª Promotoria de Justiça',
+            10,
+            '5ª Promotoria de Justiça',
+            21,
+            28,
+            149,
+            3,
+            70,
+            26,
+            4,
+            64.38848920863309,
+            0.0,
+            16.55629139072848,
+            18.461538461538463,
+            0.0,
+            0.20134228187919462,
+            0.0,
+            -0.6428571428571429,
+            -0.5384615384615384,
+            0.0)
+        expected_resp = dict(zip(field_names, expected_data))
 
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
         _run_query.assert_called_with(
-            radar_query.format(orgao_id=1, schema=settings.TABLE_NAMESPACE)
+            radar_query.format(schema=settings.TABLE_NAMESPACE),
+            parameters={"orgao_id": 1}
         )
-        self.assertEqual(resp.data, expected_data)
+        self.assertEqual(resp.data, expected_resp)
 
     @mock.patch("dominio.radar_views.run_query")
     def test_404_response(self, _run_query):
