a527cbdd132a395b31f3391d7870c55ee62920c3
rhenanbartels <rhenan.bartels@gmail.com>
2020-03-12 16:05:07 -0300

    Adiciona teste para paginator com resposta vazia

--- dominio/tests/test_mixins.py
+++ dominio/tests/test_mixins.py
@@ -1,5 +1,7 @@
 from unittest import mock, TestCase
 
+from django.core.paginator import EmptyPage
+
 from dominio.mixins import PaginatorMixin
 
 
@@ -17,3 +19,17 @@ class TestMixins(TestCase):
         page_mock.page.assert_called_once_with(1)
 
         self.assertEqual(resp, 'object list')
+
+    @mock.patch('dominio.mixins.Paginator')
+    def test_paginate_mixin_empty_list(self, _Paginator):
+        paginator = PaginatorMixin()
+        page_mock = mock.MagicMock()
+        page_mock.page.side_effect = EmptyPage
+        _Paginator.return_value = page_mock
+
+        resp = paginator.paginate([],  page=1, page_size=2)
+
+        _Paginator.assert_called_once_with([], 2)
+        page_mock.page.assert_called_once_with(1)
+
+        self.assertEqual(resp, [])
