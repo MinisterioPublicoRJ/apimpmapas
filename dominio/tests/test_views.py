3da38bbb35b55557972a0647107def522ba0689c
rhenanbartels <rhenan.bartels@gmail.com>
2020-03-23 13:28:34 -0300

    Adicona teste no caso de usário já ter logado uma vez

--- dominio/tests/test_views.py
+++ dominio/tests/test_views.py
@@ -21,3 +21,21 @@ class TestLogin(TestCase):
         self.assertEqual(resp.status_code, 200)
         _auth_integra.assert_called()
         self.assertEqual(resp.json(), expected_data)
+
+    @mock.patch("dominio.views.authenticate_integra")
+    def test_user_already_logged_in(self, _auth_integra):
+        _auth_integra.return_value = {"orgao": 12345}
+        url = reverse("dominio:login")
+
+        make("dominio.Usuario", orgao_id=12345)
+
+        resp = self.client.post(url)
+        expected_data = {
+            "first_login": False,
+            "first_login_today": True,
+            "orgao": 12345,
+        }
+
+        self.assertEqual(resp.status_code, 200)
+        _auth_integra.assert_called()
+        self.assertEqual(resp.json(), expected_data)
