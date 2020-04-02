76d4568c629cd30d41db94e103aa8efc7975ab3a
Felipe Gomes Vieira Ferreira <felipe.gomes.ferreira@gmail.com>
2020-04-02 16:57:56 -0300

    testes diminuindo workers e aumentando threads

--- app.sh
+++ app.sh
@@ -3,7 +3,7 @@ if [ -z "$WORKER" ]
 then
     # GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
     ./manage.py collectstatic --noinput
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn mprj_api.wsgi:application --workers=45 --bind=0.0.0.0:8080 --log-file -
+    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn mprj_api.wsgi:application --workers=24 --threads = 2 --bind=0.0.0.0:8080 --log-file -
 else
     celery -A lupa.tasks worker -l info --pool=solo
 fi
