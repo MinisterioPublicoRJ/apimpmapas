49ddc2545b752db2654b7a9e5f801ffe689d68db
rhenanbartels <rhenan.bartels@gmail.com>
2020-04-22 14:35:14 -0300

    Serializa dados

--- dominio/pip/dao.py
+++ dominio/pip/dao.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from dominio.db_connectors import execute as impala_execute
+from dominio.utils import format_text
 
 
 QUERIES_DIR = settings.BASE_DIR.child("dominio", "pip", "queries")
@@ -8,6 +9,42 @@ QUERIES_DIR = settings.BASE_DIR.child("dominio", "pip", "queries")
 
 class PIPRadarPerformanceDAO:
     query_file = "pip_radar_performance.sql"
+    columns = [
+        "aisp_codigo",
+        "aisp_nome",
+        "orgao_id",
+        "nr_denuncias",
+        "nr_cautelares",
+        "nr_acordos_n_persecucao",
+        "nr_arquivamentos",
+        "nr_aberturas_vista",
+        "max_aisp_denuncias",
+        "max_aisp_cautelares",
+        "max_aisp_acordos",
+        "max_aisp_arquivamentos",
+        "max_aisp_aberturas_vista",
+        "perc_denuncias",
+        "perc_cautelares",
+        "perc_acordos",
+        "perc_arquivamentos",
+        "perc_aberturas_vista",
+        "med_aisp_denuncias",
+        "med_aisp_cautelares",
+        "med_aisp_acordos",
+        "med_aisp_arquivamentos",
+        "med_aisp_aberturas_vista",
+        "var_med_denuncias",
+        "var_med_cautelares",
+        "var_med_acordos",
+        "var_med_arquivamentos",
+        "var_med_aberturas_vista",
+        "dt_calculo",
+        "nm_max_denuncias",
+        "nm_max_cautelares",
+        "nm_max_acordos",
+        "nm_max_arquivamentos",
+        "nm_max_abeturas_vista",
+    ]
 
     @classmethod
     def execute(cls, **kwargs):
@@ -15,3 +52,12 @@ class PIPRadarPerformanceDAO:
             query = fobj.read()
 
         return impala_execute(query, kwargs)
+
+    @classmethod
+    def serialize(cls, result_set):
+        ser_data = dict(zip(cls.columns, result_set[0]))
+        for column, value in ser_data.items():
+            if column.startswith("nm_max"):
+                ser_data[column] = format_text(value)
+
+        return ser_data
--- dominio/pip/tests/test_dao.py
+++ dominio/pip/tests/test_dao.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import mock
 
 from dominio.pip.dao import PIPRadarPerformanceDAO, QUERIES_DIR
@@ -15,3 +16,79 @@ class TestPIPRadarPerformance:
         _impalaa_execute.assert_called_once_with(
             query, {"orgao_id": orgao_id}
         )
+
+    def test_serialize_result(self):
+        result_set = [(
+            16,
+            '16',
+            29933850,
+            3,
+            2,
+            0,
+            0,
+            12,
+            15,
+            6,
+            0,
+            6,
+            486,
+            0.42857142857142855,
+            0.3333333333333333,
+            None,
+            0.0,
+            0.030864197530864196,
+            3.0,
+            5.0,
+            0.0,
+            0.0,
+            79.0,
+            0.0,
+            -0.6,
+            None,
+            None,
+            -0.810126582278481,
+            datetime(2020, 4, 22, 13, 36, 6, 668000),
+            '1ª PROMOTORIA DE JUSTIÇA',
+            '2ª PROMOTORIA DE JUSTIÇA',
+            '3ª PROMOTORIA DE JUSTIÇA',
+            '4ª PROMOTORIA DE JUSTIÇa',
+            '5ª PROMOTORIA DE JUSTIÇA',
+        ), ]
+        ser_data = PIPRadarPerformanceDAO.serialize(result_set)
+        expected_data = {
+            "aisp_codigo": 16,
+            "aisp_nome": '16',
+            "orgao_id": 29933850,
+            "nr_denuncias": 3,
+            "nr_cautelares": 2,
+            "nr_acordos_n_persecucao": 0,
+            "nr_arquivamentos": 0,
+            "nr_aberturas_vista": 12,
+            "max_aisp_denuncias": 15,
+            "max_aisp_cautelares": 6,
+            "max_aisp_acordos": 0,
+            "max_aisp_arquivamentos": 6,
+            "max_aisp_aberturas_vista": 486,
+            "perc_denuncias": 0.42857142857142855,
+            "perc_cautelares": 0.3333333333333333,
+            "perc_acordos": None,
+            "perc_arquivamentos": 0.0,
+            "perc_aberturas_vista": 0.030864197530864196,
+            "med_aisp_denuncias": 3.0,
+            "med_aisp_cautelares": 5.0,
+            "med_aisp_acordos": 0.0,
+            "med_aisp_arquivamentos": 0.0,
+            "med_aisp_aberturas_vista": 79.0,
+            "var_med_denuncias": 0.0,
+            "var_med_cautelares": -0.6,
+            "var_med_acordos": None,
+            "var_med_arquivamentos": None,
+            "var_med_aberturas_vista": -0.810126582278481,
+            "dt_calculo": datetime(2020, 4, 22, 13, 36, 6, 668000),
+            "nm_max_denuncias": "1ª Promotoria de Justiça",
+            "nm_max_cautelares": "2ª Promotoria de Justiça",
+            "nm_max_acordos": "3ª Promotoria de Justiça",
+            "nm_max_arquivamentos": "4ª Promotoria de Justiça",
+            "nm_max_abeturas_vista": "5ª Promotoria de Justiça",
+        }
+        assert ser_data == expected_data
