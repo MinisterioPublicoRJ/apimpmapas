0f8a078608a44b4f7dd0fed5ed1abcca8c374271
rhenanbartels <rhenan.bartels@gmail.com>
2020-04-27 15:41:14 -0300

    Busca dados no endpoint do proxy

--- proxies/detran/tests/test_views.py
+++ proxies/detran/tests/test_views.py
@@ -1,11 +1,37 @@
+from unittest import mock
+
+from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 
 
 class TestDetranProxyView(TestCase):
-    def test_correct_response(self):
-        url = reverse("proxies:foto-detran", kwargs={"rg": "12345"})
+    @mock.patch("proxies.detran.views.ImpalaGate")
+    @mock.patch("proxies.detran.views.HBaseGate")
+    @mock.patch("proxies.detran.views.DataTrafficController")
+    def test_correct_response(self, _DataController, _HBase, _Impala):
+        _HBase.return_value = "hbase object"
+        _Impala.return_value = "impala object"
+        controller_mock = mock.Mock()
+        controller_mock.get_data.return_value = {"data": 1}
+        _DataController.return_value = controller_mock
 
+        rg = "12345"
+        url = reverse("proxies:foto-detran", kwargs={"rg": rg})
         resp = self.client.get(url)
 
+        _DataController.assert_called_once_with(
+            rg=rg,
+            data_dao=_Impala.return_value,
+            photo_dao=_HBase.return_value,
+        )
+        _Impala.assert_called_once_with(
+            table_name=settings.IMPALA_DETRAN_TABLE,
+        )
+        _HBase.assert_called_once_with(
+            table_name=settings.HBASE_DETRAN_BASE,
+            server=settings.HBASE_SERVER,
+            timeout=settings.HBASE_TIMEOUT
+        )
         assert resp.status_code == 200
+        assert resp.json() == {"data": 1}
--- proxies/detran/views.py
+++ proxies/detran/views.py
@@ -1,5 +1,19 @@
+from django.conf import settings
 from django.http import JsonResponse
+
+from proxies.detran.dao import DataTrafficController, HBaseGate, ImpalaGate
 
 
 def foto_detran_view(request, rg):
-    return JsonResponse(data=dict())
+    hbase_gate = HBaseGate(
+        table_name=settings.HBASE_DETRAN_BASE,
+        server=settings.HBASE_SERVER,
+        timeout=settings.HBASE_TIMEOUT,
+    )
+    impala_gate = ImpalaGate(table_name=settings.IMPALA_DETRAN_TABLE)
+    data_controller = DataTrafficController(
+        rg=rg,
+        data_dao=impala_gate,
+        photo_dao=hbase_gate,
+    )
+    return JsonResponse(data=data_controller.get_data())
