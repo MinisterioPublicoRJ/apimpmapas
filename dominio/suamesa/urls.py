e0819c09f2ab596f8257aee424ebab7812e4c6de
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-05-21 16:39:39 -0300

    Muda URL do endpoint

--- dominio/suamesa/urls.py
+++ dominio/suamesa/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
-from .views import (
-    SuaMesaView
-)
+from dominio.suamesa.views import SuaMesaView
 
 
 urlpatterns = [
     path(
-        "suamesa/<str:orgao_id>",
+        "documentos/<str:orgao_id>",
         SuaMesaView.as_view(),
-        name="suamesa-teste",
+        name="suamesa-documentos",
     )
 ]
