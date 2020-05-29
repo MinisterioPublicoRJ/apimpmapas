7a55834a538f3b53519748aec05fcab3484a7294
Estevan Barbará <estevan.barbara@gmail.com>
2020-05-29 10:38:07 -0300

    DAO genérico de acesso ao Mongo
    Co-authored-by: rhenanbartels <rhenan.bartels@gmail.com>

--- dominio/pip/dao_integra.py
+++ dominio/pip/dao_integra.py
@@ -1,12 +1,25 @@
-from decouple import config
 from pymongo import MongoClient
+
+from mprj_api import settings
 
 
 class GenericMongoDAO:
+    uri = None
+    collection_name = None
+
     @classmethod
     def db_connect(cls):
-        client = MongoClient()
+        client = MongoClient(cls.uri)
+        return client[cls.collection_name]
+
+    @classmethod
+    def get(cls, query):
+        collection = cls.db_connect()
+        return collection.aggregate(query)
 
 
 class PIPRankingDenunciasIntegraDAO(GenericMongoDAO):
+    uri = settings.MONGO_INTEGRA_URI
+    collection_name = settings.MONGO_INTEGRA_DB
+
     pass
