c57732cd48870660dbd60a1bc2ee5672e9ae5a5f
rhenanbartels <rhenan.bartels@gmail.com>
2020-05-29 08:37:37 -0300

    Grava log de login dominio

--- dominio/login/integra.py
+++ dominio/login/integra.py
@@ -1,12 +1,22 @@
+import logging
 import jwt
 from django.conf import settings
 
 from login.jwtlogin import get_jwt_from_post, tipo_orgao
 
 
+login_logger = logging.getLogger(__name__)
+
+
 def authenticate_integra(request):
     token_part = get_jwt_from_post(request)
     payload = jwt.decode(token_part, verify=False)
+    login_logger.error(
+        f"Login efetuado pelo órgão ->"
+        f" {payload['scaUser']['nomeOrgaoUsuario']}"
+        f" : {payload['scaUser']['nomeOrgao']}"
+        f" : {payload['scaUser']['orgaoUsuario']}"
+    )
 
     user_name = payload['user_name']
     cpf = payload['scaUser']['cpfUsuario']
