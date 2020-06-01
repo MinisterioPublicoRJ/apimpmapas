aa6869fe913c9f2f2ebbe5c49e1ce1b3e83809a7
ebarbara <estevan.barbara@gmail.com>
2020-06-01 09:24:04 -0300

    Correcao query basica

--- dominio/pip/dao_integra.py
+++ dominio/pip/dao_integra.py
@@ -5,22 +5,26 @@ from mprj_api import settings
 
 class GenericMongoDAO:
     uri = None
+    database_name = None
     collection_name = None
 
     @classmethod
     def db_connect(cls):
         client = MongoClient(cls.uri)
-        return client[cls.collection_name]
+        return client[cls.database_name]
+
 
     @classmethod
     def get(cls, query):
-        collection = cls.db_connect()
+        db = cls.db_connect()
+        collection = db[cls.collection_name]
         return collection.find(query)
 
 
 class PIPRankingDenunciasIntegraDAO(GenericMongoDAO):
     uri = settings.MONGO_INTEGRA_URI
-    collection_name = settings.MONGO_INTEGRA_DB
+    database_name = settings.MONGO_INTEGRA_DB
+    collection_name = 'ip_procedimentos_stage'
 
     @classmethod
     def get_assuntos_orgao(cls, orgao_id):
