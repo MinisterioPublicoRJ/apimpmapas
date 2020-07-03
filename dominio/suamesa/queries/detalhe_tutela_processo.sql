52e750794fefc1564737f9028d841a3336bb6e45
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-07-03 15:15:06 -0300

    Explicita colunas e ordem em que devem ser pegas

--- dominio/suamesa/queries/detalhe_tutela_processo.sql
+++ dominio/suamesa/queries/detalhe_tutela_processo.sql
@@ -1,3 +1,8 @@
-SELECT *
+SELECT 
+    orgao_id,
+    nm_orgao,
+    nr_acoes_ultimos_60_dias,
+    variacao_12_meses,
+    nr_acoes_ultimos_30_dias
 FROM {schema}.tb_detalhe_processo
 WHERE orgao_id = :orgao_id
