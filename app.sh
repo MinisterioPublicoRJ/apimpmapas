b558010c9ae18686b32dc03650fdddbd92f37e5b
Felipe Gomes Vieira Ferreira <felipe.gomes.ferreira@gmail.com>
2020-07-03 17:36:18 -0300

    BUG remocao do collectstatic

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 if [ -z "$WORKER" ]
 then
     # GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
-    ./manage.py collectstatic --noinput
+    # ./manage.py collectstatic --noinput
     NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn mprj_api.wsgi:application --workers=24 --threads=2 --bind=0.0.0.0:8080 --log-file -
 else
     celery -A lupa.tasks worker -l info --pool=solo
