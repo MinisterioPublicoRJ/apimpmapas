471e27f8a0ea20c5fd4ebccb2c227831da558f6c
rhenanbartels <rhenan.bartels@gmail.com>
2020-07-24 12:35:11 -0300

    PEP8

--- dominio/login/tasks.py
+++ dominio/login/tasks.py
@@ -5,11 +5,13 @@ from dominio.login.arcgis import ARCGIS_TOKEN_CACHE_KEY, get_token
 from mprj_api.celeryconfig import app
 
 
-@app.task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 5, 'countdown': 2})
+@app.task(
+    autoretry_for=(Exception,), retry_kwargs={"max_retries": 5, "countdown": 2}
+)
 def renew_arcgis_token():
     resp = get_token()
     cache.set(
         ARCGIS_TOKEN_CACHE_KEY,
         resp,
-        timeout=settings.ARCGIS_TOKEN_EXPIRATION * 60
+        timeout=settings.ARCGIS_TOKEN_EXPIRATION * 60,
     )
