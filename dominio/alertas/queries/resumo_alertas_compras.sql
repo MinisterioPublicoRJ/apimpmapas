957d636441c4a1afa82c59bb8491e5001f7a0eb7
Rhenan <rhenan.bartels@gmail.com>
2020-08-05 14:28:36 -0300

    Melhora e formata query de alertas de compras
    Co-authored-by: Felipe Gomes Vieira Ferreira <felipe.gomes.ferreira@gmail.com>

--- dominio/alertas/queries/resumo_alertas_compras.sql
+++ dominio/alertas/queries/resumo_alertas_compras.sql
@@ -3,7 +3,8 @@ SELECT 'COMP',
     id_orgao,
     COUNT(id_orgao)
 FROM {schema}.atualizacao_pj_pacote
-INNER JOIN {schema_alertas_compras}.compras_fora_padrao_capital ON 1 = 1 AND var_perc >= 20
+INNER JOIN {schema_alertas_compras}.compras_fora_padrao_capital ON 
+    var_perc >= 20
 WHERE UPPER(pacote_atribuicao) LIKE '%%CIDADANIA%%'
     AND orgao_codamp LIKE '%%CAPITAL%%'
     AND id_orgao = :id_orgao
