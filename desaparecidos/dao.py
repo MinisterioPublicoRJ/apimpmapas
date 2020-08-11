feb3caacb7ace00914c7f59fc1e0cdcc6ff64e8f
rhenanbartels <rhenan.bartels@gmail.com>
2020-08-11 11:00:48 -0300

    Renomeia campo

--- desaparecidos/dao.py
+++ desaparecidos/dao.py
@@ -63,7 +63,7 @@ def serialize(result_set, limit=None):
         "data_fato_cand",
         "foto_cand",
         "alvo_dt_fato",
-        "idade_cand",
+        "idade_movel_cand",
         "cor_pele_cand",
         "altura_cand",
         "bairro_cand",
