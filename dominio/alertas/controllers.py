6a02e0939dc0feca0918422f1d80b32aa7c8f653
rhenanbartels <rhenan.bartels@gmail.com>
2020-09-10 10:48:23 -0300

    Envia alerta para ouvidoria

--- dominio/alertas/controllers.py
+++ dominio/alertas/controllers.py
@@ -1,21 +1,23 @@
 from django.conf import settings
 
+from dominio.alertas.tasks import async_envia_email_ouvidoria
 from dominio.db_connectors import get_hbase_table
 
 
 class EnviaAlertaComprasOuvidoriaController:
     alerta_sigla = "COMP"
+    hbase_cf = "dados_alertas"
 
     def __init__(self, orgao_id, alerta_id):
-        self.orgao_id = orgao_id
-        self.alerta_id = alerta_id
+        self.orgao_id = str(orgao_id)
+        self.alerta_id = str(alerta_id)
 
     @property
     def get_row_key(self):
         return (
             f"alerta_ouvidoria_{self.orgao_id}_{self.alerta_sigla}_"
             f"{self.alerta_id}"
-        )
+        ).encode()
 
     @property
     def get_table(self):
@@ -38,15 +40,41 @@ class EnviaAlertaComprasOuvidoriaController:
 
     @property
     def get_row_data(self):
-        return True
+        return {
+            f"{self.hbase_cf}:orgao".encode(): self.orgao_id.encode(),
+            f"{self.hbase_cf}:alerta_id".encode(): self.alerta_id.encode(),
+            f"{self.hbase_cf}:sigla".encode(): self.alerta_sigla.encode(),
+        }
 
     def save_alerta_state(self):
         self.get_table.put(
-            self.get_row_key.encode(),
+            self.get_row_key,
             self.get_row_data
         )
+
+    def envia_email(self):
+        async_envia_email_ouvidoria.delay(
+            self.orgao_id,
+            self.alerta_sigla,
+            self.alerta_id,
+        )
+
+    def prepara_resposta(self, already_sent):
+        status = 201
+        msg = "Alerta enviado para ouvidoria com sucesso"
+
+        if already_sent:
+            status = 409
+            msg = "Este alerta já foi enviado para ouvidoria"
+
+        resp = {"detail": msg}
+
+        return resp, status
 
     def envia(self):
         already_sent = self.alerta_already_sent
         if not already_sent:
             self.save_alerta_state()
+            self.envia_email()
+
+        return self.prepara_resposta(already_sent)
--- dominio/alertas/tests/test_controllers.py
+++ dominio/alertas/tests/test_controllers.py
@@ -27,26 +27,77 @@ class TestEnviaAlertaComprasOuvidoriaController(TestCase):
         self.hbase_table_mock.scan.return_value = iter('')
         self.get_hbase_table_mock.return_value = self.hbase_table_mock
 
+        self.async_envia_email_patcher = mock.patch(
+            "dominio.alertas.controllers.async_envia_email_ouvidoria"
+        )
+        self.async_envia_email_mock = self.async_envia_email_patcher.start()
+
+        self.expected_resp = {
+            "detail": "Alerta enviado para ouvidoria com sucesso"
+        }
+        self.expected_status = 201
+
     def tearDown(self):
         self.get_hbase_table_patcher.stop()
+        self.async_envia_email_patcher.stop()
 
     def test_cria_chave_do_banco(self):
         expected = (
             f"alerta_ouvidoria_{self.orgao_id}_{self.alerta_sigla}"
             f"_{self.alerta_id}"
-        )
+        ).encode()
 
         self.assertEqual(self.controller.get_row_key, expected)
 
+    def test_cria_dados_para_hbase(self):
+        data = self.controller.get_row_data
+        cf = self.controller.hbase_cf
+        expected_data = {
+            f"{cf}:orgao".encode(): self.orgao_id.encode(),
+            f"{cf}:alerta_id".encode(): self.alerta_id.encode(),
+            f"{cf}:sigla".encode(): self.controller.alerta_sigla.encode(),
+        }
+
+        self.assertEqual(data, expected_data)
+
+    def test_envia_email(self):
+        self.controller.envia_email()
+
+        self.async_envia_email_mock.delay.assert_called_once_with(
+            self.controller.orgao_id,
+            self.controller.alerta_sigla,
+            self.controller.alerta_id,
+        )
+
+    def test_prepara_resposta_email_ainda_nao_enviado(self):
+        already_sent = False
+        resp, status = self.controller.prepara_resposta(already_sent)
+
+        self.assertEqual(
+            resp,
+            {"detail": "Alerta enviado para ouvidoria com sucesso"}
+        )
+        self.assertEqual(status, 201)
+
+    def test_prepara_resposta_email_ja_enviado(self):
+        already_sent = True
+        resp, status = self.controller.prepara_resposta(already_sent)
+
+        self.assertEqual(
+            resp,
+            {"detail": "Este alerta já foi enviado para ouvidoria"}
+        )
+        self.assertEqual(status, 409)
+
     def test_envia_para_ouvidoria_com_sucesso(self):
-        self.controller.envia()
+        resp, status = self.controller.envia()
 
         self.hbase_table_mock.scan.assert_called_once_with(
             row_prefix=self.controller.get_row_key
         )
         self.hbase_table_mock.put.assert_called_once_with(
-            self.controller.get_row_key.encode(),
-            self.controller.get_row_data  # TODO: test and create this method
+            self.controller.get_row_key,
+            self.controller.get_row_data
         )
 
         connection_str = (
@@ -58,3 +109,10 @@ class TestEnviaAlertaComprasOuvidoriaController(TestCase):
             self.get_hbase_table_mock.call_args_list,
             [mock.call(connection_str), mock.call(connection_str)]
         )
+        self.async_envia_email_mock.delay.assert_called_once_with(
+            self.controller.orgao_id,
+            self.controller.alerta_sigla,
+            self.controller.alerta_id,
+        )
+        self.assertEqual(resp, self.expected_resp)
+        self.assertEqual(status, self.expected_status)
