9a4c24be7e15df0d3f38fc597ee5cac8ee13cb33
rhenanbartels <rhenan.bartels@gmail.com>
2020-09-18 18:00:25 -0300

    Fix pysolr connection

--- proxies/solr/client.py
+++ proxies/solr/client.py
@@ -1,10 +1,20 @@
 import pysolr
 from django.conf import settings
+from requests_kerberos import HTTPKerberosAuth, REQUIRED
 
 
 class SolrClient:
     def __init__(self, zookeeper, collection):
-        self._client = pysolr.SolrCloud(zookeeper, collection, timeout=300)
+        kerberos_auth = HTTPKerberosAuth(
+            mutual_authentication=REQUIRED,
+            sanitize_mutual_error_response=False
+        )
+        self._client = pysolr.SolrCloud(
+            zookeeper,
+            collection,
+            timeout=300,
+            auth=kerberos_auth,
+        )
 
     def search(self, query, start, rows):
         return self._client.search(query, start=start, rows=rows).raw_response[
