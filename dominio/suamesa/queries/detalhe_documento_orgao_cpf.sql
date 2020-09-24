89027a560cefea981bebf1b49209de6ac54094c0
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-09-24 20:45:36 -0300

    Remove SELECT * de todas as queries, explicita colunas

--- dominio/suamesa/queries/detalhe_documento_orgao_cpf.sql
+++ dominio/suamesa/queries/detalhe_documento_orgao_cpf.sql
@@ -1,4 +1,19 @@
-SELECT *
+SELECT tipo_detalhe,
+intervalo,
+vist_orgi_orga_dk,
+pesf_cpf,
+nr_documentos_distintos_atual,
+nr_aberturas_vista_atual,
+nr_aproveitamentos_atual,
+nr_instaurados_atual,
+nr_documentos_distintos_anterior,
+nr_aberturas_vista_anterior,
+nr_aproveitamentos_anterior,
+nr_instaurados_anterior,
+variacao_documentos_distintos,
+variacao_aberturas_vista,
+variacao_aproveitamentos,
+variacao_instaurados
 FROM {schema}.tb_detalhe_documentos_orgao_cpf
 WHERE tipo_detalhe = :tipo_detalhe
 AND vist_orgi_orga_dk = :orgao_id
--- dominio/tutela/queries/tempo_tramitacao_integrado.sql
+++ dominio/tutela/queries/tempo_tramitacao_integrado.sql
@@ -1,2 +1,11 @@
-SELECT * FROM {schema}.tb_tempo_tramitacao_integrado
+SELECT id_orgao,
+nome_regra,
+media_orgao,
+minimo_orgao,
+maximo_orgao,
+mediana_orgao,
+media_pacote,
+maximo_pacote,
+mediana_pacote 
+FROM {schema}.tb_tempo_tramitacao_integrado
 WHERE id_orgao = :orgao_id
