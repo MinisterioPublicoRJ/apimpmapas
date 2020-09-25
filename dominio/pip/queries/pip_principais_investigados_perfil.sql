33e7dd39ca3feba4a40189e21016726c47a8917e
rhenanbartels <rhenan.bartels@gmail.com>
2020-09-25 13:38:12 -0300

    Troca 'pesj_nm_pessoa' para 'pesj_nm_pessoa_juridica'

--- dominio/pip/queries/pip_principais_investigados_perfil.sql
+++ dominio/pip/queries/pip_principais_investigados_perfil.sql
@@ -1,4 +1,12 @@
-SELECT pess_dk, pesf_nm_pessoa_fisica, pesf_nm_mae, pesf_cpf, pesf_nr_rg, pesf_dt_nasc, pesj_nm_pessoa, pesj_cnpj
+SELECT
+    pess_dk,
+    pesf_nm_pessoa_fisica,
+    pesf_nm_mae,
+    pesf_cpf,
+    pesf_nr_rg,
+    pesf_dt_nasc,
+    pesj_nm_pessoa_juridica,
+    pesj_cnpj
 FROM {schema}.tb_pip_investigados_representantes
 LEFT JOIN {schema_exadata}.mcpr_pessoa_fisica ON pess_dk = pesf_pess_dk
 LEFT JOIN {schema_exadata}.mcpr_pessoa_juridica ON pess_dk = pesj_pess_dk
