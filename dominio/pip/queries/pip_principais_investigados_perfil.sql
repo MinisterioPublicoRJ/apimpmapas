b2433498eb0b1ed44f3a8998ecb1f5d95577b1d0
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-09-28 17:55:04 -0300

    Utiliza colunas novas da tabela para otimizar query no back

--- dominio/pip/queries/pip_principais_investigados_perfil.sql
+++ dominio/pip/queries/pip_principais_investigados_perfil.sql
@@ -1,14 +1,12 @@
 SELECT
     pess_dk,
-    pesf_nm_pessoa_fisica,
-    pesf_nm_mae,
-    pesf_cpf,
-    pesf_nr_rg,
-    pesf_dt_nasc,
-    pesj_nm_pessoa_juridica,
-    pesj_cnpj
+    pess_pesf_nm_pessoa_fisica,
+    pess_pesf_nm_mae,
+    pess_pesf_cpf,
+    pess_pesf_nr_rg,
+    pess_pesf_dt_nasc,
+    pess_pesj_nm_pessoa_juridica,
+    pess_pesj_cnpj
 FROM {schema}.tb_pip_investigados_representantes
-LEFT JOIN {schema_exadata}.mcpr_pessoa_fisica ON pess_dk = pesf_pess_dk
-LEFT JOIN {schema_exadata}.mcpr_pessoa_juridica ON pess_dk = pesj_pess_dk
 WHERE representante_dk = :dk
 ORDER BY pess_dk
