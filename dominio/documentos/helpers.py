04d07b11bf544865ca4f21673467c0175b7d012e
rhenanbartels <rhenan.bartels@gmail.com>
2020-10-05 12:05:18 -0300

    Refatora função que formata pena

--- dominio/documentos/helpers.py
+++ dominio/documentos/helpers.py
@@ -2,21 +2,13 @@ from math import ceil
 
 
 def formata_pena(pena):
-    mes_txt = "mês"
-    ano_txt = "ano"
-
     ano = int(pena)
-    mes = pena - ano
+    mes = ceil((pena - ano) * 12)
 
-    mes = ceil(mes * 12)
-
-    if ano > 1:
-        ano_txt = "anos"
-    if mes > 1:
-        mes_txt = "meses"
-
+    ano_txt = "anos" if ano > 1 else "ano"
     pena_info = f"{ano} {ano_txt}"
     if mes > 0:
+        mes_txt = "meses" if mes > 1 else "mês"
         pena_info += f" e {mes} {mes_txt}"
 
     return pena_info
