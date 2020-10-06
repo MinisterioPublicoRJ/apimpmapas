e1520fb74d454d78a008829d4f1218c8daa06cd3
rhenanbartels <rhenan.bartels@gmail.com>
2020-10-06 16:44:29 -0300

    Corrige testes do messager

--- dominio/alertas/tests/test_messages.py
+++ dominio/alertas/tests/test_messages.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.template import Context
 from django.test import TestCase
 
@@ -14,16 +16,22 @@ class TestMensagemAlertaComprasOuvidoria(TestCase):
             self.alerta_id
         )
 
-        self.expected_message = (
-            f"<html>\n  <body>\n    {self.orgao_id} - {self.alerta_id}"
-            "\n  </body>\n</html>\n"
-        )
+        self.expected_message = "msg"
         self.expected_context = Context(
             {
                 "orgao_id": self.orgao_id,
                 "alerta_id": self.alerta_id,
             }
         )
+
+        self.template_obj_mock = mock.Mock()
+        self.template_obj_mock.render.return_value = self.expected_message
+        self.template_patcher = mock.patch("dominio.alertas.messages.Template")
+        self.template_mock = self.template_patcher.start()
+        self.template_mock.return_value = self.template_obj_mock
+
+    def tearDown(self):
+        self.template_patcher.stop()
 
     def test_get_message_context(self):
         context = self.messager.context
@@ -33,4 +41,8 @@ class TestMensagemAlertaComprasOuvidoria(TestCase):
     def test_render_message(self):
         msg = self.messager.render()
 
+        self.template_mock.assert_called()
+        self.template_obj_mock.render.assert_called_once_with(
+            context=self.messager.context
+        )
         self.assertEqual(msg, self.expected_message)
