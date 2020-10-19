433e60db98c36b6d3ef272dc0f5829940ce2b89b
rhenanbartels <rhenan.bartels@gmail.com>
2020-10-19 10:07:29 -0300

    Lista e formata ROs faltantes

--- dominio/alertas/acoes/helpers.py
+++ dominio/alertas/acoes/helpers.py
@@ -1,10 +1,24 @@
+from datetime import date
+
+
 def lista_procs_ausentes(lista_procs, key_name="proc_numero_serial"):
     values = [row[key_name] for row in lista_procs]
-    return [v for v in range(max(values)) if v not in values]
+    return [v for v in range(1, max(values)) if v not in values]
 
 
 def formata_ros_ausentes(lista_numeros, num_delegacia, ano=None):
+    if ano is None:
+        ano = date.today().year
+
     return [
         f"{str(num_delegacia).zfill(3)}-{str(num).zfill(5)}/{ano}"
         for num in lista_numeros
     ]
+
+
+def ros_ausentes(lista_numeros, num_delegacia, ano=None):
+    return formata_ros_ausentes(
+        lista_procs_ausentes(lista_numeros),
+        num_delegacia=num_delegacia,
+        ano=ano
+    )
--- dominio/alertas/acoes/tests/test_helpers.py
+++ dominio/alertas/acoes/tests/test_helpers.py
@@ -1,12 +1,15 @@
 from unittest import TestCase
 
+from freezegun import freeze_time
+
 from dominio.alertas.acoes.helpers import (
     formata_ros_ausentes,
     lista_procs_ausentes,
+    ros_ausentes,
 )
 
 
-class TestListaROsAusentes(TestCase):
+class TestListaNumeroROsAusentes(TestCase):
     def setUp(self):
         self.lista_procs = [
             {"proc_numero_serial": 10},
@@ -15,7 +18,7 @@ class TestListaROsAusentes(TestCase):
             {"proc_numero_serial": 44},
         ]
         self.expected_proc_numeros = (
-            list(range(10))
+            list(range(1, 10))
             + [11]
             + list(range(13, 23))
             + list(range(24, 44))
@@ -47,3 +50,42 @@ class TestFormataListaROs(TestCase):
         )
 
         self.assertEqual(ros_formatados, self.expected_ros)
+
+    @freeze_time("2019-01-01")
+    def test_utiliza_ano_corrente(self):
+        self.expected_ros = [
+            "012-00001/2019",
+            "012-00010/2019",
+            "012-00100/2019",
+        ]
+        ros_formatados = formata_ros_ausentes(
+            self.numeros_ausentes,
+            self.num_delegacia
+        )
+        self.assertEqual(ros_formatados, self.expected_ros)
+
+
+class TestListaROsAusentes(TestCase):
+    def setUp(self):
+        self.num_delegacia = 12
+        self.numeros_existentes = [
+            {"proc_numero_serial": 1},
+            {"proc_numero_serial": 3},
+            {"proc_numero_serial": 6},
+        ]
+        self.ano = 2020
+
+        self.expected_ros = [
+            "012-00002/2020",
+            "012-00004/2020",
+            "012-00005/2020",
+        ]
+
+    def test_get_lista_ros_ausentes(self):
+        ros = ros_ausentes(
+            self.numeros_existentes,
+            self.num_delegacia,
+            self.ano
+        )
+
+        self.assertEqual(ros, self.expected_ros)
