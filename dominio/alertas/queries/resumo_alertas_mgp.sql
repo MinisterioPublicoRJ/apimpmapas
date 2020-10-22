859af74322522660184d36dff873af49ef027fd6
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-10-22 19:46:07 -0300

    Melhora performance da query de alertas

--- dominio/alertas/queries/resumo_alertas_mgp.sql
+++ dominio/alertas/queries/resumo_alertas_mgp.sql
@@ -1,13 +1,17 @@
 SELECT
     alrt.alrt_sigla,
-    alrt.alrt_descricao,
-    alrt.alrt_orgi_orga_dk,
     count(alrt.alrt_sigla) as "count"
 FROM {schema}.mmps_alertas alrt
-WHERE alrt.dt_partition =
-    (select MAX(dt_partition) FROM {schema}.mmps_alertas)
+WHERE alrt.dt_partition = :dt_partition
     AND alrt.alrt_orgi_orga_dk = :id_orgao
 GROUP BY
-    alrt.alrt_sigla,
-    alrt.alrt_descricao,
-    alrt.alrt_orgi_orga_dk
+    alrt.alrt_sigla
+UNION ALL
+SELECT 'COMP',
+    COUNT(1)
+FROM {schema}.atualizacao_pj_pacote
+INNER JOIN {schema_alertas_compras}.compras_fora_padrao_capital ON 
+    var_perc >= 20
+WHERE UPPER(pacote_atribuicao) LIKE '%%CIDADANIA%%'
+    AND orgao_codamp LIKE '%%CAPITAL%%'
+    AND id_orgao = :id_orgao
--- dominio/alertas/queries/validos_por_orgao_tipo.sql
+++ dominio/alertas/queries/validos_por_orgao_tipo.sql
@@ -1,19 +1,11 @@
 SELECT alrt_docu_dk,
 alrt_docu_nr_mp,
-alrt_docu_nr_externo,
-alrt_docu_etiqueta,
-alrt_docu_classe,
 alrt_docu_date,
 alrt_orgi_orga_dk,
-alrt_classe_hierarquia,
 alrt_dias_passados,
 alrt_dk,
-alrt_descricao,
-alrt_sigla,
-alrt_session,
-dt_partition
+alrt_sigla
 FROM {schema}.mmps_alertas alrt
-where alrt.dt_partition =
-    (select MAX(dt_partition) FROM {schema}.mmps_alertas)
+where alrt.dt_partition = :dt_partition
     AND alrt.alrt_orgi_orga_dk = :orgao_id
     AND alrt.alrt_sigla = :tipo_alerta
