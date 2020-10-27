59d834647aef81a6ed0e2c6118430e8809c25f8e
rhenanbartels <rhenan.bartels@gmail.com>
2020-10-27 11:09:36 -0300

    Ignora contagens de alerta de compras com 0 itens

--- dominio/alertas/queries/resumo_alertas.sql
+++ dominio/alertas/queries/resumo_alertas.sql
@@ -10,8 +10,9 @@ UNION ALL
 SELECT 'COMP',
     COUNT(1)
 FROM {schema}.atualizacao_pj_pacote
-INNER JOIN {schema_alertas_compras}.compras_fora_padrao_capital ON 
+INNER JOIN {schema_alertas_compras}.compras_fora_padrao_capital ON
     var_perc >= 20
 WHERE UPPER(pacote_atribuicao) LIKE '%%CIDADANIA%%'
     AND orgao_codamp LIKE '%%CAPITAL%%'
     AND id_orgao = :id_orgao
+HAVING COUNT(1) > 0
