fe7ed3e828aaca10b1afe328259502b3c4f6e152
rhenanbartels <rhenan.bartels@gmail.com>
2020-12-03 16:14:09 -0300

    Refatora classe de mensagens

--- dominio/alertas/messages.py
+++ dominio/alertas/messages.py
@@ -5,13 +5,28 @@ from django.template import Context, Template
 from dominio.alertas.dao import DetalheAlertaCompraDAO
 
 
-class MensagemOuvidoriaCompras:
-    alerta_sigla = "COMP"
-    template_name = "dominio/alertas/templates/email_ouvidoria.html"
+class MensagemOuvidoria:
+    alerta_sigla = None
+    template_name = None
 
     def __init__(self, orgao_id, alerta_id):
         self.orgao_id = orgao_id
         self.alerta_id = alerta_id
+
+    @property
+    def context(self):
+        raise NotImplementedError()
+
+    def render(self):
+        with open(self.template_name) as fobj:
+            template = Template(fobj.read())
+
+        return template.render(context=self.context)
+
+
+class MensagemOuvidoriaCompras(MensagemOuvidoria):
+    alerta_sigla = "COMP"
+    template_name = "dominio/alertas/templates/email_ouvidoria.html"
 
     def get_link_painel(self, contratacao):
         return settings.URL_PAINEL_COMPRAS\
@@ -25,9 +40,3 @@ class MensagemOuvidoriaCompras:
             detalhe_alerta["contratacao"]
         )
         return Context(detalhe_alerta)
-
-    def render(self):
-        with open(self.template_name) as fobj:
-            template = Template(fobj.read())
-
-        return template.render(context=self.context)
