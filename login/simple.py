46de4cb5e9a2d67dfba0dc1453af90df1b16a956
rhenanbartels <rhenan.bartels@gmail.com>
2021-01-25 14:23:42 -0300

    Adiciona argumento que diz nome de token a ser usado

--- login/simple.py
+++ login/simple.py
@@ -4,15 +4,21 @@ from django.conf import settings
 from django.core.exceptions import PermissionDenied
 
 
-def token_required(fun=None, *, name="token"):
+def token_required(
+        fun=None, *, token_conf_var="token-conf-var", name="token"
+):
     if fun is None:
-        return partial(token_required, name=name)
+        return partial(
+            token_required,
+            token_conf_var=token_conf_var,
+            name=name
+        )
 
     @wraps(fun)
     def wrapper(*args, **kwargs):
         request = args[1]
         token = request.GET.get(name)
-        if token != settings.SIMPLE_AUTH_TOKEN:
+        if token != getattr(settings, token_conf_var, None):
             raise PermissionDenied("Token incorreto ou inexistente!")
 
         return fun(*args, **kwargs)
